ebda85a951e0c86d534e70577dc9a06beb28615f
Edwin Mesa <edwinfmesa@hotmail.com>
2012-06-16 12:14:06 -0500

    Tres modificaciones prueba.php

--- prueba.php
+++ prueba.php
@@ -1,7 +1,8 @@
 <?php
     echo "este es el primer archivo php del projecto";
-    echo "otra linea PHP"
+    echo "otra linea PHP";
 
+    echo "Tercera modificacion de edwin";
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
