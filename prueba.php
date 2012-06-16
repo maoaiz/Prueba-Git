04ced70e46bcbc2a68570941895bbe45d4320c4b
Mauricio <mauricioaizaga@gmail.com>
2012-06-16 12:24:25 -0500

    Reparando errores en prueba.php (; linea 3)

--- prueba.php
+++ prueba.php
@@ -1,27 +1,16 @@
 <?php
     echo "este es el primer archivo php del projecto";
-    echo "otra linea PHP"
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
-
-
-
-<?php
-$git = "git remote add origin https://github.com/MaoAiz/Prueba.git";
-exec($git,$salida);
-foreach($salida as $line) {
-    echo "$line<br>";
-}
-
-$git = "git pull origin master";
-exec($git,$salida);
-foreach($salida as $line) {
-    echo "$line<br>";
-}
+    echo "otra linea PHP";
+    $git = "git remote add origin https://github.com/MaoAiz/Prueba.git";
+    exec($git,$salida);
+    foreach($salida as $line) {
+        echo "$line<br>";
+    }
+    $git = "git pull origin master";
+    exec($git,$salida);
+    foreach($salida as $line) {
+        echo "$line<br>";
+    }
 
 
 ?>
